12a100f702836cf09f1a7e0ae98d94b8784df18b
opacityDev <mohamed.hassan@opacity.dev>
2023-05-16 10:11:34 +0200

    Readme updated 'Authentication mapping updated in how to section'

--- src/main/java/dev/opacity/auth/controller/direct_auth_controller.java
+++ src/main/java/dev/opacity/auth/controller/direct_auth_controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.opacity.auth.lib.HttpUtils;
 import dev.opacity.auth.lib.lKeyRepository;
 import dev.opacity.auth.lib.lkey;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,9 +22,10 @@ public class direct_auth_controller {
     @RequestMapping(value="/{keyid}", method = RequestMethod.GET)
     String authenticate(HttpServletRequest request,@PathVariable("keyid") String key) {   
         List<lkey> target = keyRepo.findByContent(key);
-        if (target.size() < 0)
+        if (target.size() != 1)
             return "License key unrecognized";
-        if (request.getHeader("host").equals(target.get(0).getDomainName()))
+        String client_ip = HttpUtils.getRequestIP(request);
+        if (client_ip.equals(target.get(0).getDomainName()))
             return "Authentication succeeded!";
         else
             return "Authentication failed!";
